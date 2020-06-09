1e7728a1bcb314ec08e2b2b49f646a9556982fdd
Raymond <ajayitomisin1728@gmail.com>
2020-06-09 02:12:29 +0100

    Chnaged name from top_label to tool_bar, Added Scrollbar amd Canvas properties

--- MyMTN.py
+++ MyMTN.py
@@ -7,23 +7,31 @@ Created on Sat Jun  6 04:31:21 2020
 
 from tkinter import *
 from PIL import Image,ImageTk
-
+#Creating the foundational framework for the platform
 foundation =Tk()
 foundation.title("MyMTN")
-top_label=Label(foundation,bg="#FFD700")
-top_label.pack(fill=X)
-
+tool_bar=Frame(foundation,bg="#FFD700",borderwidth=2,relief = "flat")
+#Setting up the Tool bar               
 load = Image.open('pull_down.png')
 render = ImageTk.PhotoImage(load, master = foundation)
-btn_a = Button(top_label,image= render,relief= 'flat')
+btn_a = Button(tool_bar,image= render,relief= 'flat')
 btn_a.pack(side= 'left',anchor= 'w')
-
+#----------------------------------------------
 load_1 = Image.open('envelope.png')
 render_1 = ImageTk.PhotoImage(load_1, master = foundation)
-btn_b = Button(top_label,image= render_1,relief= 'flat')
+btn_b = Button(tool_bar,image= render_1,relief= 'flat')
 btn_b.pack(side= 'right',anchor= 'w')
-#btnn = Button(top_label,image= rendern,relief= 'flat')
-#btnn.pack(side= 'left',anchor= 'w')
-
+#closing toolbar               
+tool_bar.pack(side=TOP,fill=X)
+#----------------------------------------------------------
+#Setting up a Canvas and ScrollBar
+scroll_bar = Scrollbar(foundation)
+scroll_bar.pack(side=RIGHT, fill=Y)
+#----------------------------------------
+canvas= Canvas(foundation)
+canvas.pack()
+#Configuring both Canvas and Scrollbar
+canvas.config(yscrollcommand=scroll_bar.set)
+scroll_bar.config(command=canvas.yview)
 
 foundation.mainloop()
